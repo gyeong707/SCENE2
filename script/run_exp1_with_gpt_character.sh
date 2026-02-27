0e9b111f455ca3627f156065172172d330b5fb17
gyeong707 <kang9260@naver.com>
2026-02-27 03:30:30 +0000

    modify scripts

---
+++ script/run_exp1_with_gpt_character.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+TASKS=("character") # "("character" "plot")
+MODELS=("gpt-5.1" "gpt-5.2") # ("gpt-5.1" "gpt-5.2" "exaone-7b" "exaone-32b" "llama-7b" "llama-70b" "qwen-8b" "qwen-32b" "gpt-oss-20b")
+DATA_SEEDS=(42 43) #
+MODEL_SEEDS=(42 43 44 45 46) #(42 43)
+
+INPUT_DIR="./data"
+OUTPUT_DIR="./results"
+SAMPLING_COUNT=10 # 10
+TEMPERATURE=0.0
+
+for task in "${TASKS[@]}"; do
+    
+    if [ "$task" == "character" ]; then
+        INPUT_FILE="character_specification_s3_augmented_250225_updated.csv"
+    elif [ "$task" == "plot" ]; then
+        INPUT_FILE="plot_development_s3_augmented_250225.csv"
+    else
+        echo "Unknown task: $task"
+        continue
+    fi
+
+    echo "Task: $task | Input File: $INPUT_FILE"
+
+    for model in "${MODELS[@]}"; do
+        for d_seed in "${DATA_SEEDS[@]}"; do
+            for m_seed in "${MODEL_SEEDS[@]}"; do
+                
+                echo "----------------------------------------------------------------"
+                echo ":: Running: Model=$model | DataSeed=$d_seed | ModelSeed=$m_seed::"
+                echo "----------------------------------------------------------------"
+
+                python code/main.py \
+                    --input_path "$INPUT_DIR" \
+                    --input_file "$INPUT_FILE" \
+                    --output_path "$OUTPUT_DIR" \
+                    --task_type "$task" \
+                    --model_name "$model" \
+                    --dataset_seed "$d_seed" \
+                    --model_seed "$m_seed" \
+                    --sampling_count "$SAMPLING_COUNT" \
+                    --temperature "$TEMPERATURE" \
+                    --max_token 64 \
+                    --run_type full \
+                    --inference_type batch
+
+
+
+
+                if [ $? -ne 0 ]; then
+                    echo "Error occurred during execution."
+                    exit 1 
+                fi
+                
+                echo ""
+            done
+        done
+    done
+done
+
+echo "All experiments completed."
